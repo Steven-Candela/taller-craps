52b7a674be73aa5f09c5037022cc6f70cec7c9b9
Steven Candela <brayan.candela@correounivalle.edu.co>
2025-04-05 11:38:50 -0500

    Mostrar datos en el juego: tiros, puntaje, juegos ganados, juego perdidos

--- src/main/java/com/example/crapsgame/HelloController.java
+++ src/main/java/com/example/crapsgame/HelloController.java
@@ -4,11 +4,42 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
     @FXML
-    private Label welcomeText;
+    private Label rollScoreLabel;
+
+    @FXML
+    private Label dieScoreLabel;
+
+    @FXML
+    private Label gamesWonLabel;
+
+    @FXML
+    private Label gamesLostLabel;
+
+    private int rollScore = 0;
+    private int dieScore = 0;
+    private int gamesWon = 0;
+    private int gamesLost = 0;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+        rollScore++;
+
+        if (rollScore == 1) {
+            dieScore = 8;
+        } else if (rollScore == 2) {
+            gamesWon++;
+        } else if (rollScore == 3) {
+            gamesLost++;
+            rollScore = 0;
+            dieScore = 0;
+        }
+
+        rollScoreLabel.setText(String.valueOf(rollScore));
+        dieScoreLabel.setText(String.valueOf(dieScore));
+        gamesWonLabel.setText(String.valueOf(gamesWon));
+        gamesLostLabel.setText(String.valueOf(gamesLost));
     }
 }
